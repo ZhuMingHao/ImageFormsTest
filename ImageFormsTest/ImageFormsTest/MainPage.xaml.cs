2d767bd4d012503c889236a2e8f49fd46b00f186
Nico Zhu <v-zhumin@microsoft.com>
2021-04-20 11:45:41 +0800

    add the uri in the code behind

--- ImageFormsTest/ImageFormsTest/MainPage.xaml.cs
+++ ImageFormsTest/ImageFormsTest/MainPage.xaml.cs
@@ -13,6 +13,9 @@ namespace ImageFormsTest
         public MainPage()
         {
             InitializeComponent();
+            TitleImage.BindingContext = this;
+            TitleImage.SetBinding(ImageEffect.TextProperty, new Binding("Uri"));
         }
+        public string Uri { get; set; } = "https://res.zzzmh.cn/origin/db995d90880411ebb6edd017c2d2eca2.jpg";
     }
 }
